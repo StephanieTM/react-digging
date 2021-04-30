14c2ff8e993bed68d43cb08af99e113b784d81cb
Stephanie TM <23119906+StephanieTM@users.noreply.github.com>
2021-04-30 14:11:47 +0800

    Initial commit

---
+++ app/layout/pc/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className="app-header">
+      <div>
+        header
+      </div>
+      <div>
+        hello
+      </div>
+    </div>
+  );
+}
