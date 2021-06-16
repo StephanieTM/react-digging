a424525d5f4c2e092285941b47650d810edf8068
Stephanie.Gao <tm1997@163.com>
2021-06-16 17:34:25 +0800

    feat: add cleanup functions

--- src/web-apis/event-capture-and-event-bubbling/index.tsx
+++ src/web-apis/event-capture-and-event-bubbling/index.tsx
@@ -14,38 +14,96 @@ export default function Comp(): JSX.Element {
   }, []);
 
   useEffect(() => {
-    document.addEventListener('click', (e) => handleClick(e, 'document', false, 'normal'), false);
-    document.addEventListener('click', (e) => handleClick(e, 'document', true, 'normal'), true);
+    const documentBubbling = (e: MouseEvent) => handleClick(e, 'document', false, 'normal');
+    const documentCapture = (e: MouseEvent) => handleClick(e, 'document', true, 'normal');
+    document.addEventListener('click', documentBubbling, false);
+    document.addEventListener('click', documentCapture, true);
+
+    return () => {
+      document.removeEventListener('click', documentBubbling, false);
+      document.removeEventListener('click', documentCapture, true);
+    };
   }, [handleClick]);
 
   useEffect(() => {
     // Capture And Bubbling
-    document.getElementById('outer-box-1')?.addEventListener('click', (e) => handleClick(e, 'outer-box-1', false, 'normal'), false);
-    document.getElementById('middle-box-1')?.addEventListener('click', (e) => handleClick(e, 'middle-box-1', false, 'normal'), false);
-    document.getElementById('inner-box-1')?.addEventListener('click', (e) => handleClick(e, 'inner-box-1', false, 'normal'), false);
-    document.getElementById('outer-box-1')?.addEventListener('click', (e) => handleClick(e, 'outer-box-1', true, 'normal'), true);
-    document.getElementById('middle-box-1')?.addEventListener('click', (e) => handleClick(e, 'middle-box-1', true, 'normal'), true);
-    document.getElementById('inner-box-1')?.addEventListener('click', (e) => handleClick(e, 'inner-box-1', true, 'normal'), true);
+
+    const outer1Bubbling = (e: MouseEvent) => handleClick(e, 'outer-box-1', false, 'normal');
+    const middle1Bubbling = (e: MouseEvent) => handleClick(e, 'middle-box-1', false, 'normal');
+    const inner1Bubbling = (e: MouseEvent) => handleClick(e, 'inner-box-1', false, 'normal');
+    const outer1Capture = (e: MouseEvent) => handleClick(e, 'outer-box-1', true, 'normal');
+    const middle1Capture = (e: MouseEvent) => handleClick(e, 'middle-box-1', true, 'normal');
+    const inner1Capture = (e: MouseEvent) => handleClick(e, 'inner-box-1', true, 'normal');
+
+    document.getElementById('outer-box-1')?.addEventListener('click', outer1Bubbling, false);
+    document.getElementById('middle-box-1')?.addEventListener('click', middle1Bubbling, false);
+    document.getElementById('inner-box-1')?.addEventListener('click', inner1Bubbling, false);
+    document.getElementById('outer-box-1')?.addEventListener('click', outer1Capture, true);
+    document.getElementById('middle-box-1')?.addEventListener('click', middle1Capture, true);
+    document.getElementById('inner-box-1')?.addEventListener('click', inner1Capture, true);
+
+    return () => {
+      document.getElementById('outer-box-1')?.removeEventListener('click', outer1Bubbling, false);
+      document.getElementById('middle-box-1')?.removeEventListener('click', middle1Bubbling, false);
+      document.getElementById('inner-box-1')?.removeEventListener('click', inner1Bubbling, false);
+      document.getElementById('outer-box-1')?.removeEventListener('click', outer1Capture, true);
+      document.getElementById('middle-box-1')?.removeEventListener('click', middle1Capture, true);
+      document.getElementById('inner-box-1')?.removeEventListener('click', inner1Capture, true);
+    };
   }, [handleClick]);
 
   useEffect(() => {
     // Outer Stop Capture Propagation
-    document.getElementById('outer-box-2')?.addEventListener('click', (e) => handleClick(e, 'outer-box-2', false, 'normal'), false);
-    document.getElementById('middle-box-2')?.addEventListener('click', (e) => handleClick(e, 'middle-box-2', false, 'normal'), false);
-    document.getElementById('inner-box-2')?.addEventListener('click', (e) => handleClick(e, 'inner-box-2', false, 'normal'), false);
-    document.getElementById('outer-box-2')?.addEventListener('click', (e) => handleClick(e, 'outer-box-2', true, 'stopPropagation'), true);
-    document.getElementById('middle-box-2')?.addEventListener('click', (e) => handleClick(e, 'middle-box-2', true, 'normal'), true);
-    document.getElementById('inner-box-2')?.addEventListener('click', (e) => handleClick(e, 'inner-box-2', true, 'normal'), true);
+
+    const outer2Bubbling = (e: MouseEvent) => handleClick(e, 'outer-box-2', false, 'normal');
+    const middle2Bubbling = (e: MouseEvent) => handleClick(e, 'middle-box-2', false, 'normal');
+    const inner2Bubbling = (e: MouseEvent) => handleClick(e, 'inner-box-2', false, 'normal');
+    const outer2Capture = (e: MouseEvent) => handleClick(e, 'outer-box-2', true, 'stopPropagation');
+    const middle2Capture = (e: MouseEvent) => handleClick(e, 'middle-box-2', true, 'normal');
+    const inner2Capture = (e: MouseEvent) => handleClick(e, 'inner-box-2', true, 'normal');
+
+    document.getElementById('outer-box-2')?.addEventListener('click', outer2Bubbling, false);
+    document.getElementById('middle-box-2')?.addEventListener('click', middle2Bubbling, false);
+    document.getElementById('inner-box-2')?.addEventListener('click', inner2Bubbling, false);
+    document.getElementById('outer-box-2')?.addEventListener('click', outer2Capture, true);
+    document.getElementById('middle-box-2')?.addEventListener('click', middle2Capture, true);
+    document.getElementById('inner-box-2')?.addEventListener('click', inner2Capture, true);
+
+    return () => {
+      document.getElementById('outer-box-2')?.removeEventListener('click', outer2Bubbling, false);
+      document.getElementById('middle-box-2')?.removeEventListener('click', middle2Bubbling, false);
+      document.getElementById('inner-box-2')?.removeEventListener('click', inner2Bubbling, false);
+      document.getElementById('outer-box-2')?.removeEventListener('click', outer2Capture, true);
+      document.getElementById('middle-box-2')?.removeEventListener('click', middle2Capture, true);
+      document.getElementById('inner-box-2')?.removeEventListener('click', inner2Capture, true);
+    };
   }, [handleClick]);
 
   useEffect(() => {
     // Middle Stop Bubbling Propagation
-    document.getElementById('outer-box-3')?.addEventListener('click', (e) => handleClick(e, 'outer-box-3', false, 'normal'), false);
-    document.getElementById('middle-box-3')?.addEventListener('click', (e) => handleClick(e, 'middle-box-3', false, 'stopPropagation'), false);
-    document.getElementById('inner-box-3')?.addEventListener('click', (e) => handleClick(e, 'inner-box-3', false, 'normal'), false);
-    document.getElementById('outer-box-3')?.addEventListener('click', (e) => handleClick(e, 'outer-box-3', true, 'normal'), true);
-    document.getElementById('middle-box-3')?.addEventListener('click', (e) => handleClick(e, 'middle-box-3', true, 'normal'), true);
-    document.getElementById('inner-box-3')?.addEventListener('click', (e) => handleClick(e, 'inner-box-3', true, 'normal'), true);
+
+    const outer3Bubbling = (e: MouseEvent) => handleClick(e, 'outer-box-3', false, 'normal');
+    const middle3Bubbling = (e: MouseEvent) => handleClick(e, 'middle-box-3', false, 'stopPropagation');
+    const inner3Bubbling = (e: MouseEvent) => handleClick(e, 'inner-box-3', false, 'normal');
+    const outer3Capture = (e: MouseEvent) => handleClick(e, 'outer-box-3', true, 'normal');
+    const middle3Capture = (e: MouseEvent) => handleClick(e, 'middle-box-3', true, 'normal');
+    const inner3Capture = (e: MouseEvent) => handleClick(e, 'inner-box-3', true, 'normal');
+
+    document.getElementById('outer-box-3')?.addEventListener('click', outer3Bubbling, false);
+    document.getElementById('middle-box-3')?.addEventListener('click', middle3Bubbling, false);
+    document.getElementById('inner-box-3')?.addEventListener('click', inner3Bubbling, false);
+    document.getElementById('outer-box-3')?.addEventListener('click', outer3Capture, true);
+    document.getElementById('middle-box-3')?.addEventListener('click', middle3Capture, true);
+    document.getElementById('inner-box-3')?.addEventListener('click', inner3Capture, true);
+
+    return () => {
+      document.getElementById('outer-box-3')?.removeEventListener('click', outer3Bubbling, false);
+      document.getElementById('middle-box-3')?.removeEventListener('click', middle3Bubbling, false);
+      document.getElementById('inner-box-3')?.removeEventListener('click', inner3Bubbling, false);
+      document.getElementById('outer-box-3')?.removeEventListener('click', outer3Capture, true);
+      document.getElementById('middle-box-3')?.removeEventListener('click', middle3Capture, true);
+      document.getElementById('inner-box-3')?.removeEventListener('click', inner3Capture, true);
+    };
   }, [handleClick]);
 
   return (
