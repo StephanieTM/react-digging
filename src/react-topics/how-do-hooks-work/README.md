24e7377c41ef68cedb4f7f887ea6c1924d3a37df
Stephanie.Gao <tm1997@163.com>
2021-06-21 14:39:46 +0800

    feat: code organization

---
+++ src/react-topics/how-do-hooks-work/README.md
@@ -0,0 +1,3 @@
+# How do hooks work
+
+> via [blog](https://www.netlify.com/blog/2019/03/11/deep-dive-how-do-react-hooks-really-work/) by swyx
--- src/react-topics/how-do-hooks-work/index.tsx
+++ src/react-topics/how-do-hooks-work/index.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Button } from 'antd';
 import { tryExample1 } from './Example1/Counter';
 import { tryExample2 } from './Example2/Counter';
+import { tryExample3 } from './Example3/Counter';
+import { tryExample4 } from './Example4/Counter';
+import { tryExample4CustomHook } from './Example4/CustomHook';
 
 export default function Comp(): JSX.Element {
   return (
     <div>
       <h1>How do hooks work</h1>
-      <Button onClick={tryExample1}>Try Example1</Button>
-      <Button onClick={tryExample2}>Try Example2</Button>
+      <h2>See the console.</h2>
+      <div><Button onClick={tryExample1}>Try Example1</Button></div>
+      <div><Button onClick={tryExample2}>Try Example2</Button></div>
+      <div><Button onClick={tryExample3}>Try Example3</Button></div>
+      <div><Button onClick={tryExample4}>Try Example4</Button></div>
+      <div><Button onClick={tryExample4CustomHook}>Try Example4 Custom Hook</Button></div>
     </div>
   );
 }
